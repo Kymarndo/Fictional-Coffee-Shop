a009e3de8decf4d1a134012c3eb43190b71b4b0b
Kymarndo <49214298+Kymarndo@users.noreply.github.com>
2022-10-01 17:15:01 +1300

    search input and experimental hamburger button added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,24 +1,37 @@
-import styles from './Navbar.module.css';
-import { Link } from 'react-router-dom';
-import logo from '../../images/hands-and-paws.png'
-import search from '../../images/search.png'
+import styles from "./Navbar.module.css";
+import { Link } from "react-router-dom";
+import logo from "../../images/hands-and-paws.png";
+import search from "../../images/search.png";
+import React, { useState } from "react";
+import { Divide as Hamburger } from "hamburger-react";
 
 export default function Navbar() {
-    return (
-        <div className={styles.container}>
-            <div className={styles.section}><img src={logo} alt='' className={styles.logo}/></div>
-            <ul className={styles.section}>
+  const [isOpen, setOpen] = useState(false);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.logoSection}>
+        <img src={logo} alt="" className={styles.logo} />
+      </div>
+      {/* <ul className={styles.section}>
                 <Link to='/' className={styles.links}>Home</Link>
                 <Link to='/menu'  className={styles.links}>Menu</Link>
                 <Link to='/other-services'  className={styles.links}>Other Services</Link>
                 <Link to='/contact-us'  className={styles.links}>Contact Us</Link>
                 
-            </ul>
-            <div className={styles.section}><img src={search} alt='' className={styles.search} onClick={searchShowAndHide}/></div>
-        </div>
-    )
-}
-
-function searchShowAndHide () {
-    console.log("clicked");
+            </ul> */}
+      <div className={styles.searchSection}>
+        <img src={search} alt="" className={styles.search} />
+        <input className={styles.searchInput} />
+      </div>
+      <Hamburger rounded={true} size={48} onToggle={()=> setOpen (!isOpen)}  />
+      <div>{isOpen?<ul className={styles.section}>
+                <Link to='/' className={styles.links}>Home</Link>
+                <Link to='/menu'  className={styles.links}>Menu</Link>
+                <Link to='/other-services'  className={styles.links}>Other Services</Link>
+                <Link to='/contact-us'  className={styles.links}>Contact Us</Link>
+                
+            </ul>:null}</div>
+    </div>
+  );
 }
