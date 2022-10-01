9ab82b4fd6391916a5407073f82f8c5901bd291e
Kymarndo <49214298+Kymarndo@users.noreply.github.com>
2022-10-01 20:57:40 +1300

    added responsiveness, nav menu improved closing functionality

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,23 +2,41 @@ import styles from "./Navbar.module.css";
 import { Link } from "react-router-dom";
 import logo from "../../images/hands-and-paws.png";
 import search from "../../images/search.png";
-import React, { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { Divide as Hamburger } from "hamburger-react";
 
 export default function Navbar() {
+    const ref = useRef ();
   const [isOpen, setOpen] = useState(false);
 
+  useEffect ( () => {
+    const outOfMenuClickCheck = e => { //enables nav menu closing when clicking outside the menu
+        if (isOpen && ref.current && !ref.current.contains(e.target)){
+            setOpen(false);
+
+        } 
+    }
+
+    document.addEventListener("mousedown", outOfMenuClickCheck)
+
+    return () => { //cleanup to the above event listener
+        document.removeEventListener("mousedown", outOfMenuClickCheck)
+    }
+  }, [isOpen])
   return (
-    <div className={styles.container}>
+    <div className={styles.container} ref={ref}>
       <div className={styles.innerContainer}>
         <div className={styles.hamburgerSection}>
           <Hamburger
+          color="white"
             rounded={true}
             size={48}
-            onToggle={() => setOpen(!isOpen)}
+            toggled={isOpen} // <- Ensures hamburger animation reverts when menu closes from outside click
+            onToggle={() => setOpen(oldState => !oldState)}
+          
           />
           <div>
-            {isOpen ? (
+            {isOpen && (
               <ul className={styles.navMenu}>
                 <Link to="/" className={styles.links}>
                   Home
@@ -33,7 +51,7 @@ export default function Navbar() {
                   Contact Us
                 </Link>
               </ul>
-            ) : null}
+            )} 
           </div>
         </div>
         <div className={styles.searchSection}>
@@ -41,7 +59,7 @@ export default function Navbar() {
           <input className={styles.searchInput} placeholder="Search Something Here!"/>
         </div>
         <div className={styles.logoSection}>
-          <img src={logo} alt="" className={styles.logo} />
+            <Link to="/" ><img src={logo} alt="" className={styles.logo} /></Link>         
         </div>
       </div>
     </div>
