4e3680e18650f7ddb9a6b7292b0ecffca732a431
Kymarndo <49214298+Kymarndo@users.noreply.github.com>
2022-10-04 14:26:54 +1300

    hotfix: removed unused file

--- src/components/Home/HomeFeatures/HomeFeaturesData.js
+++ src/components/Home/HomeFeatures/HomeFeaturesData.js
@@ -1,6 +1,5 @@
 import img1 from "../../../images/oldDoggies.jpg";
 import img2 from "../../../images/friedRiceMix.jpg";
-import img3 from "../../../images/sweets.jpg";
 
 export const featuredData = [
     {image: img1, text: "Bring your pets!", button: "View Pet Menu"},
